1420d0787cc0ab5f4098788c1fef3e285ecc8b97
yas-1 <yas-1@yandex.ru>
2022-01-31 19:50:27 +0300

    some fix

--- src/main/java/harvi/controllers/HarviRestController.java
+++ src/main/java/harvi/controllers/HarviRestController.java
@@ -5,23 +5,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
-
+@RequestMapping("harvi")
 @RestController
 public class HarviRestController {
 
-    @GetMapping(value = "/getStack/{qName}")
-    @ResponseStatus(code = HttpStatus.OK, reason = "OK")
+    @GetMapping(value = "/getloop/{qName}")
+//    @ResponseStatus(code = HttpStatus.OK, reason = "OK")
     public String getLoopValue(@PathVariable("qName") String qName) {
+        System.out.println("we are here");
         if (App.queueMap.containsKey(qName)) {
             ConcurrentLinkedQueue<String> dataQuel = (ConcurrentLinkedQueue<String>) App.queueMap.get(qName);
             return dataQuel.poll();
         }
 
-        return "Queue is not exist";
+        return "Queue is not exist".toString();
     }
 
-    @GetMapping(value = "/getLoop/{qName}")
-    @ResponseStatus(code = HttpStatus.OK, reason = "OK")
+    @GetMapping(value = "/getstack/{qName}")
+//    @ResponseStatus(code = HttpStatus.OK, reason = "OK")
     public String getStackValue(@PathVariable("qName") String qName) {
         if (App.queueMap.containsKey(qName)) {
             ConcurrentLinkedQueue<String> dataQuel = (ConcurrentLinkedQueue<String>) App.queueMap.get(qName);
@@ -30,11 +31,11 @@ public class HarviRestController {
             return qVall;
         }
 
-        return "Queue is not exist";
+        return "Queue is not exist".toString();
     }
 
-    @GetMapping(value = "/putValue/{qName}")
-    @ResponseStatus(code = HttpStatus.OK, reason = "OK")
+    @GetMapping(value = "/putvalue/{qName}")
+//    @ResponseStatus(code = HttpStatus.OK, reason = "OK")
     public void putValue(@PathVariable("qName") String qName, @RequestParam("qValue") String qValue) {
 
         if (!App.queueMap.containsKey(qName)) {
